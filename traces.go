eb688b17f48281232e9fe98023224f234e8c3432
Aleksey Myasnikov <79263256394.ya@gmail.com>
2023-05-03 04:52:45 +0300

    WithLogger

--- traces.go
+++ traces.go
@@ -10,17 +10,7 @@ import (
 
 func WithTraces(l *logrus.Logger, d trace.Detailer, opts ...log.Option) ydb.Option {
 	a := adapter{l: l}
-	return ydb.MergeOptions(
-		ydb.WithTraceDriver(log.Driver(a, d, opts...)),
-		ydb.WithTraceTable(log.Table(a, d, opts...)),
-		ydb.WithTraceScripting(log.Scripting(a, d, opts...)),
-		ydb.WithTraceScheme(log.Scheme(a, d, opts...)),
-		ydb.WithTraceCoordination(log.Coordination(a, d, opts...)),
-		ydb.WithTraceRatelimiter(log.Ratelimiter(a, d, opts...)),
-		ydb.WithTraceDiscovery(log.Discovery(a, d, opts...)),
-		ydb.WithTraceTopic(log.Topic(a, d, opts...)),
-		ydb.WithTraceDatabaseSQL(log.DatabaseSQL(a, d, opts...)),
-	)
+	return ydb.WithLogger(a, d, opts...)
 }
 
 func Table(l *logrus.Logger, d trace.Detailer, opts ...log.Option) trace.Table {
